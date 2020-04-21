8672f1dbe9becfd6c4527e21a9dc72a2b06c93d2
panbeixing <panbx@aheading.com>
2020-04-21 13:41:01 +0800

    'commit'

--- app/src/main/java/com/aihua/hotfix/utils/TestUtils.java
+++ app/src/main/java/com/aihua/hotfix/utils/TestUtils.java
@@ -7,6 +7,9 @@ public class TestUtils {
 //        String content = "这是修复后的页面";
 
         //step 2 : 将这个类打包到dex文件中
+        //step 2.1 : 通过 javac -encoding utf-8 TestUtils.java 编译成class文件
+        //step 2.2 : 通过 jar cvfe dexPatch.jar a * 打包成jar文件
+        //我已经打包好了，在项目路径下。可以直接通过adb命令，push到/storage/emulated/0/设备下
         return content;
     }
 }
